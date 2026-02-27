9907585e1ae02bacbbb77e6a5737947e0a2aaa53
LFSC1400 <167816663+LFSC1400@users.noreply.github.com>
2026-02-27 16:24:00 -0300

    Add files via upload

---
+++ projeto2/categoria/add.php
@@ -0,0 +1,5 @@
+<div class="alert alert-warning" role="alert">
+    <h3>Adicionar categoria</h3>
+</div>
+
+<a href="?p=categoria/list" title="listar categoria">Voltar</a>
---
+++ projeto2/home.php
@@ -0,0 +1,3 @@
+<div class="alert alert-prymary" role="alert">
+    <h3>Seja bem vindo</h3>
+</div>
