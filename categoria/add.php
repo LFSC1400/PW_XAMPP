a26b6ee1aee7ada83c2421be4c1bc178f6c610a6
LFSC1400 <167816663+LFSC1400@users.noreply.github.com>
2026-02-27 16:26:39 -0300

    Delete categoria directory

--- categoria/add.php
+++
@@ -1,5 +0,0 @@
-<div class="alert alert-warning" role="alert">
-    <h3>Adicionar categoria</h3>
-</div>
-
-<a href="?p=categoria/list" title="listar categoria">Voltar</a>
--- categoria/list.php
+++
@@ -1,5 +0,0 @@
-<div class="alert alert-warning" role="alert">
-    <h3>Listar categoria</h3>
-</div>
-
-<a href="?p=categoria/add" title="Add categoria">+ Categoria</a>
